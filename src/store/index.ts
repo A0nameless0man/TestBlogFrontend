17523ec92ad0c9b14de9a56a1247c4f75bc066d4
huguang.nemo <huguang.nemo@bytedance.com>
2022-01-01 16:57:34 +0800

    store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,40 @@
 import { createStore } from "vuex";
 
+class User {
+  name = "";
+  id = 0;
+}
+class State {
+  loginStatus = "";
+  token: string = localStorage.getItem("token") || "";
+  user: User = new User();
+}
+
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: new State(),
+  mutations: {
+    auth_request(state) {
+      state.loginStatus = "loading";
+    },
+    auth_success(state: State, payload: [string, User]) {
+      const [token, user] = payload;
+      state.loginStatus = "success";
+      state.token = token;
+      state.user = user;
+    },
+    auth_error(state: State, msg = "error") {
+      state.loginStatus = msg;
+      state.token = "";
+      state.user = new User();
+    },
+    auth_quit(state: State) {
+      state.loginStatus = "";
+      state.token = "";
+      state.user = new User();
+    },
+  },
+  actions: {
+    Login(sotre);,
+  },
   modules: {},
 });
