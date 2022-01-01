15b13381de702446aeb511b9aba0a450691d49a5
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-01-01 18:41:44 +0800

    fix npe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { BootstrapVue } from "bootstrap-vue-3";
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 axios.defaults.baseURL = "/api";
 
-Vue.createApp(App).use(BootstrapVue).use(store).use(router).mount("#app");
+createApp(App).use(BootstrapVue).use(store).use(router).mount("#app");
